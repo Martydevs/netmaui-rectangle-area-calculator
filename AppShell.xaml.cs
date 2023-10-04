6a6de5fd2b975a6c95e69d69b2e5205c05c947fd
Martydevs <tmdev64@gmail.com>
2023-10-03 20:17:56 -0600

    initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace some_app;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace some_app;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void CalcularArea(object sender, EventArgs e)
+    {
+		if (!string.IsNullOrEmpty(Altura.Text) && !string.IsNullOrEmpty(Base.Text))
+		{
+			var localAltura = double.Parse(Altura.Text);
+			var localBase = double.Parse(Base.Text);
+			var localArea = localAltura * localBase;
+
+			Resultado.Text = localArea.ToString();
+		} else
+		{
+			Resultado.Text = "Llene todos los campos";
+		}
+    }
+
+    private void LimpiarControles(object sender, EventArgs e)
+    {
+		Altura.Text = "";
+		Base.Text = "";
+		Resultado.Text = "";
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Application.Current.Quit();
+    }
+}
+
